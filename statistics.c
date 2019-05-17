AzusaOS/net-tools

commit 614e15d6f4e32d094288414f6bf086d9761539f8

statistics: fix multiple typos in strings

po/cs.po
@@ -1584,7 +1584,7 @@ msgstr "Počet v pořádku znovu sestavených paketů: %u"
 
 #: ../statistics.c:81 ../statistics.c:105
 #, c-format
-msgid "%u packet reassembles failed"
+msgid "%u packet reassemblies failed"
 msgstr "Počet paketů, jež se nepodařilo znovu sestavit: %u"
 
 #: ../statistics.c:82 ../statistics.c:106
@@ -1634,7 +1634,7 @@ msgstr "Počet přijatých ICMP zpráv: %u"
 
 #: ../statistics.c:116 ../statistics.c:146
 #, c-format
-msgid "%u input ICMP message failed."
+msgid "%u input ICMP message failed"
 msgstr "Počet chybných příchozích ICMP zpráv: %u"
 
 #: ../statistics.c:117 ../statistics.c:130 ../statistics.c:147
@@ -1725,7 +1725,7 @@ msgstr "Počet přesměrování: %u"
 
 #: ../statistics.c:135
 #, c-format
-msgid "echo request: %u"
+msgid "echo requests: %u"
 msgstr "Žádost o echo: %u"
 
 #: ../statistics.c:137
@@ -1815,7 +1815,7 @@ msgstr "RTO algoritmus je %s"
 
 #: ../statistics.c:184
 #, c-format
-msgid "%u active connections openings"
+msgid "%u active connection openings"
 msgstr "Počet aktivně navázaných spojení: %u"
 
 #: ../statistics.c:185
@@ -1845,17 +1845,17 @@ msgstr "Počet přijatých segmentů: %u"
 
 #: ../statistics.c:190
 #, c-format
-msgid "%u segments send out"
+msgid "%u segments sent out"
 msgstr "Počet odeslaných segmentů: %u"
 
 #: ../statistics.c:191
 #, c-format
-msgid "%u segments retransmited"
+msgid "%u segments retransmitted"
 msgstr "Počet přenesených segmentů: %u"
 
 #: ../statistics.c:192
 #, c-format
-msgid "%u bad segments received."
+msgid "%u bad segments received"
 msgstr "Počet chybných příchozích segmentů: %u"
 
 #: ../statistics.c:193
@@ -1870,7 +1870,7 @@ msgstr "Počet přijatých paketů: %u"
 
 #: ../statistics.c:199 ../statistics.c:207
 #, c-format
-msgid "%u packets to unknown port received."
+msgid "%u packets to unknown port received"
 msgstr "Počet paketů přijatých pro neznámý port: %u"
 
 #: ../statistics.c:200 ../statistics.c:208
@@ -1965,7 +1965,7 @@ msgstr "Počet odmítnutých aktivních spojení kvůli časovému razítku: %u"
 
 #: ../statistics.c:237
 #, c-format
-msgid "%u packets rejects in established connections because of timestamp"
+msgid "%u packetes rejected in established connections because of timestamp"
 msgstr ""
 "Počet odmítnutých packetů uvnitř navázaného spojení kvůli časovému razítku: %"
 "u"
@@ -1998,7 +1998,7 @@ msgstr "Počet zahozených SYN packetů určených naslouchajícímu socketu: %u
 
 #: ../statistics.c:246
 #, c-format
-msgid "%u packets directly queued to recvmsg prequeue."
+msgid "%u packets directly queued to recvmsg prequeue"
 msgstr "Počet packetů přímo zařazených do předfronty recvmsg: %u"
 
 #: ../statistics.c:248
@@ -2023,7 +2023,7 @@ msgstr "Počet předpovězených hlaviček packetů: %u"
 
 #: ../statistics.c:253
 #, c-format
-msgid "%u packets header predicted and directly queued to user"
+msgid "%u packet headers predicted and directly queued to user"
 msgstr "Počet hlaviček packetů předpovězených a přímo předaných uživateli: %u"
 
 #: ../statistics.c:255

po/et_EE.po
@@ -1349,7 +1349,7 @@ msgstr "%u paketti pandi edukalt kokku"
 
 #: ../statistics.c:80
 #, c-format
-msgid "%u packet reassembles failed"
+msgid "%u packet reassemblies failed"
 msgstr "%u pakettide kokkupanekut ebaõnnestus"
 
 #: ../statistics.c:81
@@ -1374,7 +1374,7 @@ msgstr "%u ICMP teadet võeti vastu"
 
 #: ../statistics.c:89
 #, c-format
-msgid "%u input ICMP message failed."
+msgid "%u input ICMP message failed"
 msgstr "%u sisenevat ICMP teadet olid vigased"
 
 #: ../statistics.c:90 ../statistics.c:103
@@ -1463,7 +1463,7 @@ msgstr "ümbersuunamisi: %u"
 
 #: ../statistics.c:108
 #, c-format
-msgid "echo request: %u"
+msgid "echo requests: %u"
 msgstr " echo päringuid: %u"
 
 #: ../statistics.c:110
@@ -1488,7 +1488,7 @@ msgstr "RTO algoritm on %s"
 
 #: ../statistics.c:122
 #, c-format
-msgid "%u active connections openings"
+msgid "%u active connection openings"
 msgstr "%u aktiivset ühenduse avamist"
 
 #: ../statistics.c:123
@@ -1518,17 +1518,17 @@ msgstr "%u segmenti võeti vastu"
 
 #: ../statistics.c:128
 #, c-format
-msgid "%u segments send out"
+msgid "%u segments sent out"
 msgstr "%u segmenti saadeti"
 
 #: ../statistics.c:129
 #, c-format
-msgid "%u segments retransmited"
+msgid "%u segments retransmitted"
 msgstr "%u segmenti saadeti uuesti"
 
 #: ../statistics.c:130
 #, c-format
-msgid "%u bad segments received."
+msgid "%u bad segments received"
 msgstr "%u vigast segmenti võeti vastu"
 
 #: ../statistics.c:131
@@ -1543,7 +1543,7 @@ msgstr "%u paketti võeti vastu"
 
 #: ../statistics.c:137
 #, c-format
-msgid "%u packets to unknown port received."
+msgid "%u packets to unknown port received"
 msgstr "%u sisenevat paketti tundmatule pordile"
 
 #: ../statistics.c:138
@@ -1631,7 +1631,7 @@ msgstr "%u aktiivsest ühendusest keelduti ajatempli tõttu"
 
 #: ../statistics.c:167
 #, c-format
-msgid "%u packets rejects in established connections because of timestamp"
+msgid "%u packetes rejected in established connections because of timestamp"
 msgstr "%u paketist keelduti loodud ühendustes ajatempli tõttu"
 
 #: ../statistics.c:169
@@ -1661,7 +1661,7 @@ msgstr "%u SYN'i kuulavatele soklitele ignoreeriti"
 
 #: ../statistics.c:176
 #, c-format
-msgid "%u packets directly queued to recvmsg prequeue."
+msgid "%u packets directly queued to recvmsg prequeue"
 msgstr "%u paketti pandi otse recvmsg eeljärjekorda"
 
 #: ../statistics.c:178
@@ -1681,12 +1681,12 @@ msgstr "%u paketti kustutati eeljärjekorrast"
 
 #: ../statistics.c:183
 #, c-format
-msgid "%u packets header predicted"
+msgid "%u packet headers predicted"
 msgstr "%u paketti päise õige ennustusega"
 
 #: ../statistics.c:184
 #, c-format
-msgid "%u packets header predicted and directly queued to user"
+msgid "%u packet headers predicted and directly queued to user"
 msgstr "%u paketti päise õige ennustusega pandi otse kasutaja järjekorda"
 
 #: ../statistics.c:186

po/net-tools.pot
@@ -1465,7 +1465,7 @@ msgstr ""
 
 #: ../statistics.c:80
 #, c-format
-msgid "%u packet reassembles failed"
+msgid "%u packet reassemblies failed"
 msgstr ""
 
 #: ../statistics.c:81
@@ -1490,7 +1490,7 @@ msgstr ""
 
 #: ../statistics.c:89
 #, c-format
-msgid "%u input ICMP message failed."
+msgid "%u input ICMP message failed"
 msgstr ""
 
 #: ../statistics.c:90 ../statistics.c:103
@@ -1579,7 +1579,7 @@ msgstr ""
 
 #: ../statistics.c:108
 #, c-format
-msgid "echo request: %u"
+msgid "echo requests: %u"
 msgstr ""
 
 #: ../statistics.c:110
@@ -1604,7 +1604,7 @@ msgstr ""
 
 #: ../statistics.c:122
 #, c-format
-msgid "%u active connections openings"
+msgid "%u active connection openings"
 msgstr ""
 
 #: ../statistics.c:123
@@ -1634,17 +1634,17 @@ msgstr ""
 
 #: ../statistics.c:128
 #, c-format
-msgid "%u segments send out"
+msgid "%u segments sent out"
 msgstr ""
 
 #: ../statistics.c:129
 #, c-format
-msgid "%u segments retransmited"
+msgid "%u segments retransmitted"
 msgstr ""
 
 #: ../statistics.c:130
 #, c-format
-msgid "%u bad segments received."
+msgid "%u bad segments received"
 msgstr ""
 
 #: ../statistics.c:131
@@ -1659,7 +1659,7 @@ msgstr ""
 
 #: ../statistics.c:137
 #, c-format
-msgid "%u packets to unknown port received."
+msgid "%u packets to unknown port received"
 msgstr ""
 
 #: ../statistics.c:138
@@ -1746,7 +1746,7 @@ msgstr ""
 
 #: ../statistics.c:167
 #, c-format
-msgid "%u packets rejects in established connections because of timestamp"
+msgid "%u packetes rejected in established connections because of timestamp"
 msgstr ""
 
 #: ../statistics.c:169
@@ -1776,7 +1776,7 @@ msgstr ""
 
 #: ../statistics.c:176
 #, c-format
-msgid "%u packets directly queued to recvmsg prequeue."
+msgid "%u packets directly queued to recvmsg prequeue"
 msgstr ""
 
 #: ../statistics.c:178
@@ -1801,7 +1801,7 @@ msgstr ""
 
 #: ../statistics.c:183
 #, c-format
-msgid "%u packets header predicted and directly queued to user"
+msgid "%u packet headers predicted and directly queued to user"
 msgstr ""
 
 #: ../statistics.c:185

statistics.c
@@ -70,7 +70,7 @@ static const struct entry Iptab[] =
     {"OutDiscards", N_("%llu outgoing packets dropped"), opt_number},
     {"OutNoRoutes", N_("%llu dropped because of missing route"), opt_number},
     {"OutRequests", N_("%llu requests sent out"), number},	/*? */
-    {"ReasmFails", N_("%llu packet reassembles failed"), opt_number},
+    {"ReasmFails", N_("%llu packet reassemblies failed"), opt_number},
     {"ReasmOKs", N_("%llu packets reassembled ok"), opt_number},
     {"ReasmReqds", N_("%llu reassemblies required"), opt_number},	/* ? */
     {"ReasmTimeout", N_("%llu fragments dropped after timeout"), opt_number},
@@ -96,7 +96,7 @@ static const struct entry Ip6tab[] =
     {"Ip6OutMcastPkts", N_("%llu outgoing multicast packets"), opt_number},
     {"Ip6OutNoRoutes", N_("%llu dropped because of missing route"), opt_number},
     {"Ip6OutRequests", N_("%llu requests sent out"), number},     /*? */
-    {"Ip6ReasmFails", N_("%llu packet reassembles failed"), opt_number},
+    {"Ip6ReasmFails", N_("%llu packet reassemblies failed"), opt_number},
     {"Ip6ReasmOKs", N_("%llu packets reassembled ok"), opt_number},
     {"Ip6ReasmReqds", N_("%llu reassemblies required"), opt_number}, /* ? */
     {"Ip6ReasmTimeout", N_("%llu fragments dropped after timeout"), opt_number},
@@ -109,7 +109,7 @@ static const struct entry Icmptab[] =
     {"InDestUnreachs", N_("destination unreachable: %llu"), i_inp_icmp | I_TITLE},
     {"InEchoReps", N_("echo replies: %llu"), i_inp_icmp | I_TITLE},
     {"InEchos", N_("echo requests: %llu"), i_inp_icmp | I_TITLE},
-    {"InErrors", N_("%llu input ICMP message failed."), number},
+    {"InErrors", N_("%llu input ICMP message failed"), number},
     {"InMsgs", N_("%llu ICMP messages received"), number},
     {"InParmProbs", N_("wrong parameters: %llu"), i_inp_icmp | I_TITLE},	/*? */
     {"InRedirects", N_("redirects: %llu"), i_inp_icmp | I_TITLE},
@@ -121,7 +121,7 @@ static const struct entry Icmptab[] =
     {"OutAddrMasks", N_("address mask requests: %llu"), i_outp_icmp | I_TITLE},
     {"OutDestUnreachs", N_("destination unreachable: %llu"), i_outp_icmp | I_TITLE},
     {"OutEchoReps", N_("echo replies: %llu"), i_outp_icmp | I_TITLE},
-    {"OutEchos", N_("echo request: %llu"), i_outp_icmp | I_TITLE},
+    {"OutEchos", N_("echo requests: %llu"), i_outp_icmp | I_TITLE},
     {"OutErrors", N_("%llu ICMP messages failed"), number},
     {"OutMsgs", N_("%llu ICMP messages sent"), number},
     {"OutParmProbs", N_("wrong parameters: %llu"), i_outp_icmp | I_TITLE},	/*? */
@@ -137,7 +137,7 @@ static const struct entry Icmp6tab[] =
     {"Icmp6InDestUnreachs", N_("destination unreachable: %llu"), i_inp_icmp | I_TITLE},
     {"Icmp6InEchoReplies", N_("echo replies: %llu"), i_inp_icmp | I_TITLE},
     {"Icmp6InEchos", N_("echo requests: %llu"), i_inp_icmp | I_TITLE},
-    {"Icmp6InErrors", N_("%llu input ICMP message failed."), number},
+    {"Icmp6InErrors", N_("%llu input ICMP message failed"), number},
     {"Icmp6InGroupMembQueries", N_("group member queries: %llu"), i_inp_icmp | I_TITLE},
     {"Icmp6InGroupMembReductions", N_("group member reductions: %llu"), i_inp_icmp | I_TITLE},
     {"Icmp6InGroupMembResponses", N_("group member responses: %llu"), i_inp_icmp | I_TITLE},
@@ -169,17 +169,17 @@ static const struct entry Icmp6tab[] =
 
 static const struct entry Tcptab[] =
 {   /* Keep the entries sorted! */
-    {"ActiveOpens", N_("%llu active connections openings"), number},
+    {"ActiveOpens", N_("%llu active connection openings"), number},
     {"AttemptFails", N_("%llu failed connection attempts"), number},
     {"CurrEstab", N_("%llu connections established"), number},
     {"EstabResets", N_("%llu connection resets received"), number},
-    {"InErrs", N_("%llu bad segments received."), number},
+    {"InErrs", N_("%llu bad segments received"), number},
     {"InSegs", N_("%llu segments received"), number},
     {"MaxConn", "", number},
     {"OutRsts", N_("%llu resets sent"), number},
-    {"OutSegs", N_("%llu segments send out"), number},
+    {"OutSegs", N_("%llu segments sent out"), number},
     {"PassiveOpens", N_("%llu passive connection openings"), number},
-    {"RetransSegs", N_("%llu segments retransmited"), number},
+    {"RetransSegs", N_("%llu segments retransmitted"), number},
     {"RtoAlgorithm", N_("RTO algorithm is %s"), i_rto_alg | I_STATIC},
     {"RtoMax", "", number},
     {"RtoMin", "", number},
@@ -189,7 +189,7 @@ static const struct entry Udptab[] =
 {   /* Keep the entries sorted! */
     {"InDatagrams", N_("%llu packets received"), number},
     {"InErrors", N_("%llu packet receive errors"), number},
-    {"NoPorts", N_("%llu packets to unknown port received."), number},
+    {"NoPorts", N_("%llu packets to unknown port received"), number},
     {"OutDatagrams", N_("%llu packets sent"), number},
     {"RcvbufErrors", N_("%llu receive buffer errors"), number},
     {"SndbufErrors", N_("%llu send buffer errors"), number},
@@ -199,7 +199,7 @@ static const struct entry Udp6tab[] =
 {   /* Keep the entries sorted! */
     {"Udp6InDatagrams", N_("%llu packets received"), number},
     {"Udp6InErrors", N_("%llu packet receive errors"), number},
-    {"Udp6NoPorts", N_("%llu packets to unknown port received."), number},
+    {"Udp6NoPorts", N_("%llu packets to unknown port received"), number},
     {"Udp6OutDatagrams", N_("%llu packets sent"), number},
 };
 
@@ -215,7 +215,7 @@ static const struct entry Tcpexttab[] =
     {"OfoPruned", N_("%llu packets dropped from out-of-order queue because of socket buffer overrun"), opt_number},
     {"OutOfWindowIcmps", N_("%llu ICMP packets dropped because they were out-of-window"), opt_number},
     {"PAWSActive", N_("%llu active connections rejected because of time stamp"), opt_number},
-    {"PAWSEstab", N_("%llu packets rejects in established connections because of timestamp"), opt_number},
+    {"PAWSEstab", N_("%llu packetes rejected in established connections because of timestamp"), opt_number},
     {"PAWSPassive", N_("%llu passive connections rejected because of time stamp"), opt_number},
     {"PruneCalled", N_("%llu packets pruned from receive queue because of socket buffer overrun"), opt_number},
     {"RcvPruned", N_("%llu packets pruned from receive queue"), opt_number},
@@ -244,14 +244,14 @@ static const struct entry Tcpexttab[] =
     {"TCPFullUndo", N_("%llu congestion windows fully recovered without slow start"), opt_number},
     {"TCPHPAcks", N_("%llu predicted acknowledgments"), opt_number},
     {"TCPHPHits", N_("%llu packet headers predicted"), number},
-    {"TCPHPHitsToUser", N_("%llu packets header predicted and directly queued to user"), opt_number},
+    {"TCPHPHitsToUser", N_("%llu packet headers predicted and directly queued to user"), opt_number},
     {"TCPLossFailures",  N_("%llu timeouts in loss state"), opt_number},
     {"TCPLoss", N_("%llu TCP data loss events"), opt_number},
     {"TCPLossUndo", N_("%llu congestion windows recovered without slow start after partial ack"), opt_number},
     {"TCPLostRetransmits", N_("%llu retransmits lost"), opt_number},
     {"TCPMemoryPressures", N_("TCP ran low on memory %llu times"), opt_number},
     {"TCPPartialUndo", N_("%llu congestion windows partially recovered using Hoe heuristic"), opt_number},
-    {"TCPPrequeued", N_("%llu packets directly queued to recvmsg prequeue."), opt_number},
+    {"TCPPrequeued", N_("%llu packets directly queued to recvmsg prequeue"), opt_number},
     {"TCPPrequeueDropped", N_("%llu packets dropped from prequeue"), opt_number},
     {"TCPPureAcks", N_("%llu acknowledgments not containing data payload received"), opt_number},
     {"TCPRcvCollapsed", N_("%llu packets collapsed in receive queue due to low socket buffer"), opt_number},

po/de.po
@@ -1518,7 +1518,7 @@ msgstr "%d Fragmente korrekt empfangen"
 
 #: ../statistics.c:78
 #, c-format
-msgid "%d packet reassembles failed"
+msgid "%d packet reassemblies failed"
 msgstr "%d fehlgeschlagene Paketdefragmentierungen"
 
 #: ../statistics.c:79
@@ -1543,7 +1543,7 @@ msgstr "%d ICMP Nachrichten empfangen"
 
 #: ../statistics.c:87
 #, c-format
-msgid "%d input ICMP message failed."
+msgid "%d input ICMP message failed"
 msgstr "%d eingegangene ICMP Nachrichten fehlgeschlagen"
 
 #: ../statistics.c:88 ../statistics.c:101
@@ -1631,7 +1631,7 @@ msgstr "Umleitungen: %d"
 
 #: ../statistics.c:106
 #, c-format
-msgid "echo request: %d"
+msgid "echo requests: %d"
 msgstr "Echo Anfragen: %d"
 
 #: ../statistics.c:108
@@ -1661,7 +1661,7 @@ msgstr "RTO Algorithmus is %s"
 
 #: ../statistics.c:120
 #, c-format
-msgid "%d active connections openings"
+msgid "%d active connection openings"
 msgstr "%d Verbindungen aktiv geöffnet"
 
 #: ../statistics.c:121
@@ -1691,18 +1691,18 @@ msgstr "%d Segmente empfangen"
 
 #: ../statistics.c:126
 #, c-format
-msgid "%d segments send out"
+msgid "%d segments sent out"
 msgstr "%d Segmente abgeschickt"
 
 #: ../statistics.c:127
 #, c-format
-msgid "%d segments retransmited"
+msgid "%d segments retransmitted"
 msgstr "%d Segmente erneut geschickt"
 
 #: ../statistics.c:128
 #, c-format
-msgid "%d bad segments received."
-msgstr "%d fehlerhafte Segmente empfangen."
+msgid "%d bad segments received"
+msgstr "%d fehlerhafte Segmente empfangen"
 
 #: ../statistics.c:129
 #, c-format
@@ -1716,8 +1716,8 @@ msgstr "%d Pakete empfangen"
 
 #: ../statistics.c:135
 #, c-format
-msgid "%d packets to unknown port received."
-msgstr "%d Pakete für unbekannte Ports empfangen."
+msgid "%d packets to unknown port received"
+msgstr "%d Pakete für unbekannte Ports empfangen"
 
 #: ../statistics.c:136
 #, c-format

po/fr.po
@@ -1536,7 +1536,7 @@ msgstr "%d paquets réassemblés correctement"
 
 #: ../statistics.c:78
 #, c-format
-msgid "%d packet reassembles failed"
+msgid "%d packet reassemblies failed"
 msgstr "%d paquets mal réassemblés"
 
 #: ../statistics.c:79
@@ -1561,7 +1561,7 @@ msgstr "%d messages ICMP reçus"
 
 #: ../statistics.c:87
 #, c-format
-msgid "%d input ICMP message failed."
+msgid "%d input ICMP message failed"
 msgstr "%d messages d'entrée ICMP en échec"
 
 #: ../statistics.c:88 ../statistics.c:101
@@ -1649,7 +1649,7 @@ msgstr "redirection: %d"
 
 #: ../statistics.c:106
 #, c-format
-msgid "echo request: %d"
+msgid "echo requests: %d"
 msgstr "requête echo: %d"
 
 #: ../statistics.c:108
@@ -1679,7 +1679,7 @@ msgstr "algorithme RTO est %s"
 
 #: ../statistics.c:120
 #, c-format
-msgid "%d active connections openings"
+msgid "%d active connection openings"
 msgstr "%d ouvertures de connexions actives"
 
 #: ../statistics.c:121
@@ -1709,18 +1709,18 @@ msgstr "%d segments reçus"
 
 #: ../statistics.c:126
 #, c-format
-msgid "%d segments send out"
+msgid "%d segments sent out"
 msgstr "%d segments envoyés"
 
 #: ../statistics.c:127
 #, c-format
-msgid "%d segments retransmited"
+msgid "%d segments retransmitted"
 msgstr "%d segments retransmis"
 
 #: ../statistics.c:128
 #, c-format
-msgid "%d bad segments received."
-msgstr "%d mauvais segments reçus."
+msgid "%d bad segments received"
+msgstr "%d mauvais segments reçus"
 
 #: ../statistics.c:129
 #, c-format
@@ -1734,7 +1734,7 @@ msgstr "%d paquets reçus"
 
 #: ../statistics.c:135
 #, c-format
-msgid "%d packets to unknown port received."
+msgid "%d packets to unknown port received"
 msgstr "%d paquets reçus pour un port inconnu"
 
 #: ../statistics.c:136

po/pt_BR.po
@@ -1530,7 +1530,7 @@ msgstr "%d pacotes remontados sem problemas"
 
 #: ../statistics.c:78
 #, c-format
-msgid "%d packet reassembles failed"
+msgid "%d packet reassemblies failed"
 msgstr "%d remontagens de pacotes falharam"
 
 #: ../statistics.c:79
@@ -1555,8 +1555,8 @@ msgstr "%d mensagens ICMP recebidas"
 
 #: ../statistics.c:87
 #, c-format
-msgid "%d input ICMP message failed."
-msgstr "%d mensagens ICMP entrantes falharam."
+msgid "%d input ICMP message failed"
+msgstr "%d mensagens ICMP entrantes falharam"
 
 #: ../statistics.c:88 ../statistics.c:101
 #, c-format
@@ -1643,7 +1643,7 @@ msgstr "redireções: %d"
 
 #: ../statistics.c:106
 #, c-format
-msgid "echo request: %d"
+msgid "echo requests: %d"
 msgstr "requisições de eco: %d"
 
 #: ../statistics.c:108
@@ -1673,7 +1673,7 @@ msgstr "Algorítmo RTO é %s"
 
 #: ../statistics.c:120
 #, c-format
-msgid "%d active connections openings"
+msgid "%d active connection openings"
 msgstr "%d tentativas de conexão falharam"
 
 #: ../statistics.c:121
@@ -1703,18 +1703,18 @@ msgstr "%d segmentos recebidos"
 
 #: ../statistics.c:126
 #, c-format
-msgid "%d segments send out"
+msgid "%d segments sent out"
 msgstr "%d segmentos enviados"
 
 #: ../statistics.c:127
 #, c-format
-msgid "%d segments retransmited"
+msgid "%d segments retransmitted"
 msgstr "%d segmentos retransmitidos"
 
 #: ../statistics.c:128
 #, c-format
-msgid "%d bad segments received."
-msgstr "%d segmentos ruins recebidos."
+msgid "%d bad segments received"
+msgstr "%d segmentos ruins recebidos"
 
 #: ../statistics.c:129
 #, c-format
@@ -1728,8 +1728,8 @@ msgstr "%d pacotes recebidos"
 
 #: ../statistics.c:135
 #, c-format
-msgid "%d packets to unknown port received."
-msgstr "%d pacotes para portas desconhecidas recebidos."
+msgid "%d packets to unknown port received"
+msgstr "%d pacotes para portas desconhecidas recebidos"
 
 #: ../statistics.c:136
 #, c-format

commit 97eb14d7eef14019054bdcf0bbb97b16983a3298

statistics: fix packets typo
Commit 614e15d6 introduced a typo of "packets" to "packetes". Correct
spelling where required.

Signed-off-by: Jamie Bainbridge <jamie.bainbridge@gmail.com>

po/cs.po
@@ -1960,7 +1960,7 @@ msgstr "Počet odmítnutých aktivních spojení kvůli časovému razítku: %u"
 
 #: ../statistics.c:237
 #, c-format
-msgid "%u packetes rejected in established connections because of timestamp"
+msgid "%u packets rejected in established connections because of timestamp"
 msgstr ""
 "Počet odmítnutých packetů uvnitř navázaného spojení kvůli časovému razítku: %"
 "u"

po/et_EE.po
@@ -1626,7 +1626,7 @@ msgstr "%u aktiivsest ühendusest keelduti ajatempli tõttu"
 
 #: ../statistics.c:167
 #, c-format
-msgid "%u packetes rejected in established connections because of timestamp"
+msgid "%u packets rejected in established connections because of timestamp"
 msgstr "%u paketist keelduti loodud ühendustes ajatempli tõttu"
 
 #: ../statistics.c:169

po/net-tools.pot
@@ -1746,7 +1746,7 @@ msgstr ""
 
 #: ../statistics.c:167
 #, c-format
-msgid "%u packetes rejected in established connections because of timestamp"
+msgid "%u packets rejected in established connections because of timestamp"
 msgstr ""
 
 #: ../statistics.c:169

statistics.c
@@ -215,7 +215,7 @@ static const struct entry Tcpexttab[] =
     {"OfoPruned", N_("%llu packets dropped from out-of-order queue because of socket buffer overrun"), opt_number},
     {"OutOfWindowIcmps", N_("%llu ICMP packets dropped because they were out-of-window"), opt_number},
     {"PAWSActive", N_("%llu active connections rejected because of time stamp"), opt_number},
-    {"PAWSEstab", N_("%llu packetes rejected in established connections because of timestamp"), opt_number},
+    {"PAWSEstab", N_("%llu packets rejected in established connections because of timestamp"), opt_number},
     {"PAWSPassive", N_("%llu passive connections rejected because of time stamp"), opt_number},
     {"PruneCalled", N_("%llu packets pruned from receive queue because of socket buffer overrun"), opt_number},
     {"RcvPruned", N_("%llu packets pruned from receive queue"), opt_number},